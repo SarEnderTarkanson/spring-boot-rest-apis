9ddc0bbf983772372ba326fa92203bbb7a9898a1
Alparslan Tarkan <atarkan@hotmail.com>
2025-12-19 06:44:21 +0100

    section 2 - create entities

--- 01-books/src/main/java/com/luv2code/books/controller/BookController.java
+++ 01-books/src/main/java/com/luv2code/books/controller/BookController.java
@@ -1,13 +1,34 @@
 package com.luv2code.books.controller;
 
+import com.luv2code.books.entity.Book;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class BookController {
 
-    @GetMapping("/api")
-    public String firstAPI() {
-        return "Hello Eric!";
+    private final List<Book> books = new ArrayList<>();
+
+    public BookController() {
+        initializeBooks();
+    }
+
+    private void initializeBooks() {
+        books.addAll(List.of(
+                new Book("Title One", "Author One", "Science"),
+                new Book("Title Two", "Author Two", "History"),
+                new Book("Title Three", "Author Three", "Math"),
+                new Book("Title Four", "Author Four", "Science"),
+                new Book("Title Five", "Author Five", "History"),
+                new Book("Title Six", "Author Six", "Math")
+        ));
+    }
+
+    @GetMapping("/api/books")
+    public List<Book> getBooks() {
+        return books;
     }
 }
