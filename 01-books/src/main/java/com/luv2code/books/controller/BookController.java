dca958841c958329e1005ad6489eefc76028d350
Alparslan Tarkan <atarkan@hotmail.com>
2025-12-22 06:41:29 +0100

    section 2 - path variables with streams

--- 01-books/src/main/java/com/luv2code/books/controller/BookController.java
+++ 01-books/src/main/java/com/luv2code/books/controller/BookController.java
@@ -35,12 +35,10 @@ public class BookController {
 
     @GetMapping("api/books/{title}")
     public Book getBookByTitle(@PathVariable String title) {
-        for (Book book : books) {
-            if (book.getTitle().equalsIgnoreCase(title)) {
-                return book;
-            }
-        }
-        return null;
+        return books.stream()
+                .filter(book -> book.getTitle().equalsIgnoreCase(title))
+                .findFirst()
+                .orElse(null);
     }
 
 }
