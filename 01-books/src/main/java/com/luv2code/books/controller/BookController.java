219f3a2a587288bbe3271209bef0a7fc0d3f1729
Alparslan Tarkan <atarkan@hotmail.com>
2025-12-22 16:56:05 +0100

    section 2 - request mapping

--- 01-books/src/main/java/com/luv2code/books/controller/BookController.java
+++ 01-books/src/main/java/com/luv2code/books/controller/BookController.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/books")
 public class BookController {
 
     private final List<Book> books = new ArrayList<>();
@@ -26,7 +27,7 @@ public class BookController {
         ));
     }
 
-    @GetMapping("/api/books")
+    @GetMapping
     public List<Book> getSomeBook(@RequestParam(required = false) String category) {
 
         if (category == null) {
@@ -41,7 +42,7 @@ public class BookController {
     }
 
 
-    @GetMapping("api/books/{title}")
+    @GetMapping("/{title}")
     public Book getBookByTitle(@PathVariable String title) {
         return books.stream()
                 .filter(book -> book.getTitle().equalsIgnoreCase(title))
@@ -49,7 +50,7 @@ public class BookController {
                 .orElse(null);
     }
 
-    @PostMapping("/api/books")
+    @PostMapping
     public void createBook(@RequestBody Book newbook) {
 
         boolean isNewBook = books.stream()
@@ -61,7 +62,7 @@ public class BookController {
 
     }
 
-    @PutMapping("/api/books/{title}")
+    @PutMapping("/{title}")
     public void updateBook(@PathVariable String title, @RequestBody Book updatedBook) {
         for (int i = 0; i < books.size(); i++) {
             if (books.get(i).getTitle().equalsIgnoreCase(title)) {
@@ -71,7 +72,7 @@ public class BookController {
         }
     }
 
-    @DeleteMapping("api/books/{title}")
+    @DeleteMapping("/{title}")
     public void deleteBook(@PathVariable String title) {
         books.removeIf(book -> book.getTitle().equalsIgnoreCase(title));
     }
