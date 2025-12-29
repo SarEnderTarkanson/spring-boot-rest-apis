0e9f68297a1abe56479e3552926aee7da26522a0
Alparslan Tarkan <atarkan@hotmail.com>
2025-12-29 16:06:49 +0100

    section 3 - global exception handling

--- 02-books/src/main/java/com/luv2code/books/controller/BookController.java
+++ 02-books/src/main/java/com/luv2code/books/controller/BookController.java
@@ -136,4 +136,14 @@ public class BookController {
         return new ResponseEntity<>(bookErrorResponse, HttpStatus.NOT_FOUND);
     }
 
+    @ExceptionHandler
+    public ResponseEntity<BookErrorResponse> handleException(Exception exc) {
+        BookErrorResponse bookErrorResponse = new BookErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                "Invalid request",
+                System.currentTimeMillis()
+        );
+        return new ResponseEntity<>(bookErrorResponse, HttpStatus.BAD_REQUEST);
+    }
+
 }
