265a1e9e1cd427499521bb02dab775ee9d831181
Alparslan Tarkan <atarkan@hotmail.com>
2026-01-02 10:54:59 +0100

    section 4 - setup employee service

--- 03-employees/src/main/java/com/luv2code/springboot/employees/controller/EmployeeRestController.java
+++ 03-employees/src/main/java/com/luv2code/springboot/employees/controller/EmployeeRestController.java
@@ -1,7 +1,7 @@
 package com.luv2code.springboot.employees.controller;
 
-import com.luv2code.springboot.employees.dao.EmployeeDAO;
 import com.luv2code.springboot.employees.entity.Employee;
+import com.luv2code.springboot.employees.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,16 +13,16 @@ import java.util.List;
 @RequestMapping("/api")
 public class EmployeeRestController {
 
-    private EmployeeDAO employeeDAO;
+    private EmployeeService employeeService;
 
     @Autowired
-    public EmployeeRestController(EmployeeDAO theEmployeeDAO) {
-        this.employeeDAO = theEmployeeDAO;
+    public EmployeeRestController(EmployeeService theEmployeeService) {
+        this.employeeService = theEmployeeService;
     }
 
     @GetMapping("/employees")
     public List<Employee> findAll() {
-        return employeeDAO.findAll();
+        return employeeService.findAll();
     }
 
 }
---
+++ 03-employees/src/main/java/com/luv2code/springboot/employees/service/EmployeeService.java
@@ -0,0 +1,11 @@
+package com.luv2code.springboot.employees.service;
+
+import com.luv2code.springboot.employees.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    List<Employee> findAll();
+
+}
