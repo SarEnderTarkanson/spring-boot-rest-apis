4018975c58448b5685a44407a26765809e925a2a
Alparslan Tarkan <atarkan@hotmail.com>
2026-01-15 09:05:31 +0100

    section 5 - 95 - spring security and swagger

--- 03-employees/src/main/java/com/luv2code/springboot/employees/security/SecurityConfig.java
+++ 03-employees/src/main/java/com/luv2code/springboot/employees/security/SecurityConfig.java
@@ -3,12 +3,14 @@ package com.luv2code.springboot.employees.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -50,9 +52,25 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
         );
 
+        http.httpBasic(AbstractHttpConfigurer::disable);
         http.httpBasic(Customizer.withDefaults());
         http.csrf(AbstractHttpConfigurer::disable);
+        http.exceptionHandling(exceptionHandling -> exceptionHandling
+                .authenticationEntryPoint(authenticationEntryPoint())
+        );
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationEntryPoint authenticationEntryPoint() {
+        return (request, response, authException) -> {
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            response.setContentType("application/json");
+
+            response.setHeader("WWW-Authenticate", "");
+
+            response.getWriter().write("{\"error\":\"Unauthorized access\"}");
+        };
     }
 
 }
