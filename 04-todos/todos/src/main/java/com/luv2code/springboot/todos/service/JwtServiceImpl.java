240a0e04e53393794fdfccaf9ff4c4c7f325a34c
Alparslan Tarkan <atarkan@hotmail.com>
2026-02-21 00:37:56 +0100

    Section 6 - 125 extract claims from jwt

--- 04-todos/todos/src/main/java/com/luv2code/springboot/todos/service/JwtServiceImpl.java
+++ 04-todos/todos/src/main/java/com/luv2code/springboot/todos/service/JwtServiceImpl.java
@@ -1,5 +1,6 @@
 package com.luv2code.springboot.todos.service;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
@@ -10,9 +11,10 @@ import org.springframework.stereotype.Service;
 import javax.crypto.SecretKey;
 import java.util.Date;
 import java.util.Map;
+import java.util.function.Function;
 
 @Service
-public class JwtServiceImpl implements  JwtService {
+public class JwtServiceImpl implements JwtService {
 
     @Value("${spring.jwt.secret}")
     private String SECRET_KEY;
@@ -22,7 +24,20 @@ public class JwtServiceImpl implements  JwtService {
 
     @Override
     public String extractUsername(String token) {
-        return "";
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
     }
 
     @Override
