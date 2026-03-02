ddf07a6036f2e6e08d8751a400c9a1c198fe0bb1
Alparslan Tarkan <atarkan@hotmail.com>
2026-03-02 08:06:29 +0100

    Section 6 - 135 authentication controller register

---
+++ 04-todos/todos/src/main/java/com/luv2code/springboot/todos/controller/AuthenticationController.java
@@ -0,0 +1,28 @@
+package com.luv2code.springboot.todos.controller;
+
+import com.luv2code.springboot.todos.request.RegisterRequest;
+import com.luv2code.springboot.todos.service.AuthenticationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@Tag(name = "Authentication REST API endpoints", description = "Operations related to register & login")
+public class AuthenticationController {
+
+    public AuthenticationController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    private final AuthenticationService authenticationService;
+
+    @Operation(summary = "Register a user", description = "Create new user in the database")
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/register")
+    public void register(@Valid @RequestBody RegisterRequest registerRequest) throws Exception {
+        authenticationService.register(registerRequest);
+    }
+}
