a5e0eb6eb18c3b284f7fe4fd5189ed8149cb0e09
Alparslan Tarkan <atarkan@hotmail.com>
2026-03-25 06:33:41 +0100

    Section 6 - 142 implement swagger configuration for bearer token

--- 04-todos/todos/src/main/java/com/luv2code/springboot/todos/config/SecurityConfig.java
+++ 04-todos/todos/src/main/java/com/luv2code/springboot/todos/config/SecurityConfig.java
@@ -59,9 +59,9 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(configurer ->
                 configurer
-                        .requestMatchers("/api/auth/**","/swagger-ui/**", "/v3/api-docs/**",
+                        .requestMatchers("/api/auth/**", "/swagger-ui/**", "/v3/api-docs/**",
                                 "/swagger-resources/**", "/webjars/**",
-                                "/docs").permitAll()
+                                "/docs").permitAll().anyRequest().authenticated()
         );
 
         http.csrf(AbstractHttpConfigurer::disable);
